f6243e79ab62adaf4e5158f1497efc29af98dd37
Nino Righi <deidrox.business@t-online.de>
2020-01-27 17:46:56 +0100

    Update Nav Hide

---
+++ js/navigation.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.querySelector("#show-nav").style.display = "none";
+    document.querySelector("#hide-nav").style.display = "initial";
+}
+
+function closeNav() {
+      document.querySelector("#hide-nav").style.display = "none";
+      document.querySelector("#show-nav").style.display = "block";
+} 
