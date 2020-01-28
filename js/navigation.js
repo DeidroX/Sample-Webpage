423040a2cced82c5bce2611c952f542738d45bcc
Nino Righi <deidrox.business@t-online.de>
2020-01-28 11:51:22 +0100

    Update Navigation transition

--- js/navigation.js
+++ js/navigation.js
@@ -1,9 +1,21 @@
 function openNav() {
-    document.querySelector("#show-nav").style.display = "none";
-    document.querySelector("#hide-nav").style.display = "initial";
+    document.querySelector("#show-nav").style.opacity = "0";
+    document.querySelector("#show-nav").style.left = "-250px";
+    document.querySelector("#show-nav").style.top = "-250px";
+    document.querySelector("#show-nav").style.visibility = "hidden";
+    document.querySelector("#hide-nav").style.visibility = "visible";
+    document.querySelector("#hide-nav").style.opacity = "100%";
+    document.querySelector("#hide-nav").style.left = "0";
+    document.querySelector("#hide-nav").style.top = "0";
 }
 
 function closeNav() {
-      document.querySelector("#hide-nav").style.display = "none";
-      document.querySelector("#show-nav").style.display = "block";
+    document.querySelector("#hide-nav").style.opacity = "0";
+    document.querySelector("#hide-nav").style.left = "-250px";
+    document.querySelector("#hide-nav").style.top = "-250px";
+    document.querySelector("#hide-nav").style.visibility = "hidden";
+    document.querySelector("#show-nav").style.visibility = "visible";
+    document.querySelector("#show-nav").style.opacity = "100%";
+    document.querySelector("#show-nav").style.left = "0";
+    document.querySelector("#show-nav").style.top = "0";
 } 
