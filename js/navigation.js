9d8ab8272f29c96f922e10ced05bd17a97958d7c
Nino Righi <deidrox.business@t-online.de>
2020-02-06 12:40:34 +0100

    Arrow slide in

--- js/navigation.js
+++ js/navigation.js
@@ -15,3 +15,18 @@ function closeNav() {
         "style", 
         "opacity: 100%; left: 0; top: 0; visibility: visible;");
 } 
+
+/* Slide in Info Arrow after 2.5 seconds */
+
+const arrow = document.querySelector(".fa-long-arrow-alt-left");
+
+setTimeout(() => {
+    arrow.classList.add('animation-target');
+    arrow.style.visibility = "visible";
+}, 2500);
+
+/* After another 2.5 seconds fade out (duration 2.5 seconds) */
+/* TODO */
+setTimeout(() => {
+    arrow.style.visibility = "visible";
+}, 2500);
